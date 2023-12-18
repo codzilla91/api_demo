f3e270dde3023c97e23659065b7258bbd65e36f9
James <jtung501@gmail.com>
2023-12-18 12:57:31 +0800

    test: added unit test

--- test/controllers/currency_converter_controller_test.rb
+++ test/controllers/currency_converter_controller_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class CurrencyConverterControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should convert currency' do
+    get '/convert', params: { source: 'USD', target: 'JPY', amount: '$1,525' }
+    assert_response :success
+
+    response_data = JSON.parse(response.body)
+    assert_equal 'success', response_data['msg']
+    assert_equal '$170,496.53', response_data['amount']
+  end
 end
